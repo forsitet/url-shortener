14b532dd62f20ac606ced7f0d111e78eec444cb7
Denis Budin <D20041152@yandex.ru>
2024-10-17 19:25:28 +0300

    added func SaveURL

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -3,8 +3,10 @@ package sqlite
 import (
 	"database/sql"
 	"fmt"
+	"github.com/mattn/go-sqlite3"
 
-	_ "github.com/mattn/go-sqlite3"
+	"url-shortener/internal/storage"
+
 )
 
 type Storage struct {
@@ -36,6 +38,29 @@ func New(storagePath string) (*Storage, error) {
 	}
 
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error){
+	const op = "storage.sqlite.SaveURL"
+
+	stmt, err := s.db.Prepare("INSERT INTO url(url, alias) VALUES(?, ?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	res, err := stmt.Exec(urlToSave, alias)
+	if err != nil{
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique{
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrURLExists)
+		}
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil{
+		return 0, fmt.Errorf("%s: failed to get last insert id: %w", op, err)
+	}
+
+	return id, nil
 }
 
 
